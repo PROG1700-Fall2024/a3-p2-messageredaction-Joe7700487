4112f5570851555054bce285ae30bdc23fd468cb
joe <w0500154@nscc.ca>
2024-11-06 14:03:16 -0400

    not finished

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -2,17 +2,37 @@
 #Description:   Design and write a program that counts and removes all desired letters from 
 #               any user-entered sentence or phrase.
 
-#Student #:     
-#Student Name:  
+#Student #:     w0500154
+#Student Name:  Joseph Petrash
+
+
+
+def getInput(phrase, badLetters):
+    phrase = input("Type a phrase (or quit to exit program): ")
+    badLetters = input("Type a comma-separated list of letters to redact: ")
+    phrase = createList(phrase)
+    badLetters = createList(badLetters)
+    return phrase, badLetters
+
+def createList(phrase):
+    letters = []
+    for i in phrase:
+        letters.append(i)
+    return letters
+
+def checkForLetters(stringList, badList):
+    pass
+
+def removeLetter(stringList, badLetter):
+    pass
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
-
-
-
-
-
+    phrase = ""
+    badLetters = ""
+    phrase, badLetters = getInput(phrase, badLetters)
+    checkForLetters(phrase, badLetters)
 
 
     # YOUR CODE ENDS HERE
